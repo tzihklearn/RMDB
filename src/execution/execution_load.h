f132a3895e01f435f91b27d0c35ab230d4a5b384
tzihklearn <1922941549@qq.com>
2024-08-08 21:55:59 +0800

    test: 解决多线程并发过程中锁的问题

--- src/execution/execution_load.h
+++ src/execution/execution_load.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <future>
+#include <utility>
 #include "execution_defs.h"
 #include "execution_manager.h"
 #include "executor_abstract.h"
@@ -80,6 +81,7 @@ public:
 //            std::cout << "load insert complete!" << std::endl;
 //            return nullptr;
 //        });
+        context_->file_name = file_name_;
         // 创建一个线程来执行异步任务
         std::thread thread([this]() {
 //            std::cout << "Thread is going to sleep for 10 seconds\n";
@@ -90,6 +92,8 @@ public:
             auto context = context_;
             auto sm_manager = sm_manager_;
             auto cols = tab_.cols;
+            auto indexes = tab_.indexes;
+            auto fh = fh_;
             std::vector<ColType> expectedTypes;
             for (auto & col : cols) {
                 expectedTypes.push_back(col.type);
@@ -118,7 +122,7 @@ public:
                 my_line = line;
 
                 // 插入数据到表中
-                insertIntoTable(table_name, values, sm_manager, context);
+                insertIntoTable(table_name, values, sm_manager, context, cols, fh, indexes);
             }
 //            for (int i = 0; i < 10000; ++i) {
 //                std::cout << &"load: " [ i] << std::endl;
@@ -129,7 +133,7 @@ public:
         // 等待线程启动，但立即返回
         thread.detach();
         std::cout << "Thread is going to sleep for 1 seconds\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // 休眠10秒
+        std::this_thread::sleep_for(std::chrono::seconds(1)); // 休眠1秒
         std::cout << "load ok!" << std::endl;
 
         // 返回一个空的std::unique_ptr，因为实际的返回值将在异步任务中处理
@@ -184,7 +188,8 @@ public:
     }
 
     // 将数据插入到表中
-    void insertIntoTable(const std::string &table_name, const std::vector<Value> &values, SmManager *sm_manager, Context *context) {
+    void insertIntoTable(const std::string &table_name, const std::vector<Value> &values, SmManager *sm_manager, Context *context,
+                         std::vector<ColMeta> cols, RmFileHandle* fh, std::vector<IndexMeta> indexes) {
         // 使用数据库系统的API或方法将数据插入到表中
 //        RmRecord rec(fh_->get_file_hdr().record_size);
 //        for (size_t i = 0; i < values.size(); i++) {
@@ -198,9 +203,59 @@ public:
 //        }
 //        fh_->insert_record(rec.data, context_);
         //insert
-        InsertExecutor insertExecutor(sm_manager, table_name, values, context);
-        insertExecutor.Next();
+//        auto context_ = new Context(context);
+//        context_->txn_->set_state(TransactionState::DEFAULT);
+//        InsertExecutor insertExecutor(sm_manager, table_name, values, context_);
+//        insertExecutor.Next();
+        insertValue(table_name, values, sm_manager, context, std::move(cols), fh, std::move(indexes));
+//        delete context_;
 //        std::cout << "insert :" << line_ << std::endl;
+    }
+
+    void insertValue(const std::string &table_name, const std::vector<Value> &values, SmManager *sm_manager, Context *context,
+                     std::vector<ColMeta> cols, RmFileHandle* fh, std::vector<IndexMeta> indexes) {
+        // 构建记录
+        RmRecord rec(fh->get_file_hdr().record_size);
+        for (size_t i = 0; i < values.size(); i++) {
+            auto &col = cols[i];
+            Value val = values[i];
+            if (col.type != val.type) {
+                throw IncompatibleTypeError(coltype2str(col.type), coltype2str(val.type));
+            }
+            val.init_raw(col.len);
+            memcpy(rec.data + col.offset, val.raw->data, col.len);
+        }
+
+        // 插入记录到文件并获取新的rid
+        rid_ = fh->insert_record(rec.data, context);
+
+        // 申请行级排他锁（X锁）
+//        context_->lock_mgr_->lock_exclusive_on_record(context_->txn_, rid_, fh_->GetFd());
+
+        // 插入索引条目
+        for (auto &index: indexes) {
+            auto ih = sm_manager->ihs_.at(
+                    sm_manager->get_ix_manager()->get_index_name(table_name, index.cols)).get();
+
+            std::vector<char> key(index.col_tot_len);
+            int offset = 0;
+            for (size_t i = 0; i < index.col_num; ++i) {
+                memcpy(key.data() + offset, rec.data + index.cols[i].offset, index.cols[i].len);
+                offset += index.cols[i].len;
+            }
+
+            try {
+                ih->insert_entry(key.data(), rid_, context->txn_);
+                auto *index_rcd = new IndexWriteRecord(WType::INSERT_TUPLE, table_name, rid_, key.data(),
+                                                       index.col_tot_len);
+                context->txn_->append_index_write_record(index_rcd);
+            } catch (InternalError &error) {
+                // 回滚操作
+                fh->delete_record(rid_, context);
+                context->txn_->set_state(TransactionState::ABORTED);
+                throw InternalError("Non-unique index!");
+            }
+        }
     }
 
     Rid &rid() override { return rid_; }
