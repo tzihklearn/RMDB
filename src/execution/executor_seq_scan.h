cab79743d8ee6862193765403ec69c72a2802db2
tzihklearn <1922941549@qq.com>
2024-08-09 22:09:38 +0800

    test: 继续测试

--- src/execution/executor_seq_scan.h
+++ src/execution/executor_seq_scan.h
@@ -43,10 +43,9 @@ public:
         context_ = context;
 
         fedConditions = conditions_;
-
+        exit(1);
         // 申请表级共享锁（S）
         if(context_!= nullptr){
-            exit(1);
             context_->lock_mgr_->lock_shared_on_table(context_->txn_, fh_->GetFd());
         }
     }
